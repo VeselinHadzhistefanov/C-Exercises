5635ca0240cf50c7f845d54f3778c9ac7a9578b8
VeselinH <v.hadzhistefanov@gmail.com>
2023-08-25 16:26:24 +0300

    Update

--- C++ Exercises 1/C++ Exercises 1.cpp
+++ C++ Exercises 1/C++ Exercises 1.cpp
@@ -2,22 +2,26 @@
 #include <string>
 using namespace std;
 
+string getDuplicates(string a, string b) {
+	string chars = "";
+
+	for (char c1 : a) {
+		if (b.find(c1) < b.length()) {
+			if (chars.find(c1) >= chars.length()) {
+				chars.append(string(1, c1));
+			}
+		}
+	}
+
+	return chars;
+}
+
+
 int main()
 {
-    for (int i = 0; i < 100; i++) {
-        
-        if (i % 3 == 0) {
-            cout << "git";
-        }
+	cout << getDuplicates("abc", "def");
+	cout << getDuplicates("abc", "cde");
+	cout << getDuplicates("abc", "dafc");
+	cout << getDuplicates("abca", "deaf");
 
-        if (i % 5 == 0) {
-            cout << "hub";
-        }
-
-        if (i % 3 != 0 && i % 5 != 0) {
-            cout << to_string(i);
-        }
-
-        cout << endl;
-    }
 }
