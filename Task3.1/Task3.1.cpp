cacf4918d6e316fce2156b16b3f6062351578d6a
Veselin <v.hadzhistefanov@gmail.com>
2023-08-28 16:51:45 +0300

    Update

---
+++ Task3.1/Task3.1.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+string convertIP(long long num) {
+
+	string s;
+	long long bytePart[4];
+
+	for (int i = 0; i < 4; i++) {
+		long long exponent = pow(2, 8 * (i + 1));
+		bytePart[i] = num % exponent;
+
+		num = (num - bytePart[i]) / 256;
+	}
+
+	for (int i = 0; i < 4; i++) {
+		s.append(to_string(bytePart[3 - i]));
+		if (i < 3) {
+			s.append(".");
+		}
+	}
+
+	return s;
+}
+
+int main() {
+	long long num = 256;
+	string s = convertIP(num);
+
+	cout << endl << s;
+
+}
