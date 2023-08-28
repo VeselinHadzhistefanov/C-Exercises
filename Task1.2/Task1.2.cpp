bb67447f485c426045d3320996e829fed76434d2
Veselin <v.hadzhistefanov@gmail.com>
2023-08-28 16:46:50 +0300

    Update

---
+++ Task1.2/Task1.2.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+string getDuplicates(string a, string b) {
+	string chars = "";
+
+	for (char c1 : a) {
+		if (b.find(c1) < b.length()) {
+			if (chars.find(c1) >= chars.length()) {
+				chars.append(string(1, c1));
+			}
+		}
+	}
+
+	return chars;
+}
+
+int main() {
+	cout << getDuplicates("abc", "def");
+	cout << getDuplicates("abc", "cde");
+	cout << getDuplicates("abc", "dafc");
+	cout << getDuplicates("abca", "deaf");
+
+}
