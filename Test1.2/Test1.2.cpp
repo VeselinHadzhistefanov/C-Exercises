7cd5887833a71294109bb89fbe1fe0f44d0f13ac
Veselin <v.hadzhistefanov@gmail.com>
2023-08-28 16:39:45 +0300

    Update

---
+++ Test1.2/Test1.2.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+/*
+ * [1, 3, 8, 1, 16, 3, 4] => [1, 3, 8, 16, 4]
+ */
+
+
+vector<int> removeDuplicates(vector<int>& arr) {
+
+	vector<int> result_arr(arr);
+
+	for (int i = 0; i < result_arr.size(); i++) {
+		int n = result_arr[i];
+
+		for (int j = i + 1; j < result_arr.size(); j++) {
+			if (result_arr[j] == n) {
+				result_arr.erase(result_arr.begin() + j);
+			}
+		}
+
+	}
+
+	return result_arr;
+}
+
+int main() {
+	vector<int> arr = { 1, 3, 8, 1, 16, 3, 4, 1, 3, 8, 1, 16, 3, 4 };
+
+	arr = removeDuplicates(arr);
+
+	for (int i : arr) {
+		cout << i << " ";
+	}
+}
