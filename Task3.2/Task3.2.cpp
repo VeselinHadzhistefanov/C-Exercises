b0c9f10c0c184f1a4c2804b3a6d663e429226231
Veselin <v.hadzhistefanov@gmail.com>
2023-08-28 16:52:41 +0300

    Update

--- Task3.2/Task3.2.cpp
+++ Task3.2/Task3.2.cpp
@@ -1,20 +1,41 @@
-// Task3.2.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <string>
+using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+/*
+"255.255.255.255" => 4228250625
+"0.0.0.1" => 1
+"0.0.1.1" => 257
+ */
+
+long long convertFromIP(string s) {
+
+	long long IPNum = 0;
+
+	for (int i = 0; i < 4; i++) {
+		const int n = s.find_last_of('.');
+		string element = s.substr(n + 1, s.size());
+
+		if (element.size() > 3 || element.size() == 0) {
+			throw(new exception("Invalid input!"));
+		}
+
+		int num = stoi(element);
+		if (num > 255 || num < 0) {
+			throw(new exception("Invalid input!"));
+		}
+
+		const long long exponent = pow(2, 8 * i);
+
+		s = s.substr(0, n);
+		IPNum += num * exponent;
+	}
+
+	return IPNum;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+int main() {
+	cout << convertFromIP("255.255.255.255") << endl;
+	cout << convertFromIP("0.0.0.1") << endl;
+	cout << convertFromIP("0.0.1.1") << endl;
+}
