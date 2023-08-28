4eb8514f15b493744e59c95cf2592afad85e4708
Veselin <v.hadzhistefanov@gmail.com>
2023-08-28 16:38:28 +0300

    Update

--- Test1.1/Test1.1.cpp
+++ Test1.1/Test1.1.cpp
@@ -15,19 +15,30 @@ vector<int> getKBiggest(vector<int>& elements, int n) {
 	for (int i = 0; i < elements.size(); i++) {
 		int element = elements[i];
 
+		if (biggest.size() == 0) {
+			biggest.push_back(element);
+			continue;
+		}
+
 		int pos = 0;
 		for (int j = 0; j < biggest.size(); j++) {
 
 			if (element < biggest[j]) {
-				pos = j;
+				pos = j + 1;
 			}
 		}
 
-		if (pos == n - 1) {
+		if (pos == n) {
 			continue;
 		}
 
-		biggest.insert(biggest.begin() + pos, element);
+		if (pos == biggest.size()) {
+			biggest.push_back(element);
+		}
+		else {
+			biggest.insert(biggest.begin() + pos, element);
+		}
+
 		if (biggest.size() > n) {
 			biggest.pop_back();
 		}
@@ -41,9 +52,9 @@ vector<int> getKBiggest(vector<int>& elements, int n) {
 int main() {
 	vector<int> elements = { 1, 8, 0, 6, 3, 9, 10, 15, 12 };
 
-	elements = getKBiggest(elements, 3);
+	elements = getKBiggest(elements, 7);
 
-	for (int i : elements) {
-		cout << i << " ";
+	for (int i = 0; i < elements.size(); i++) {
+		cout << elements[i] << " ";
 	}
 }
